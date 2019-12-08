edc20d532421c7bd6b3ae23a8de08e3dbebff424
Justin L <justinlandis1@gmail.com>
2019-12-08 13:00:13 -0600

    added a service to return the heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { HEROES } from "../mock-heroes";
 import { Hero } from "../hero";
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: "app-heroes",
@@ -8,14 +8,21 @@ import { Hero } from "../hero";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() {}
+  getHeroes(): void {
+     this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
-  ngOnInit() {}
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 }
